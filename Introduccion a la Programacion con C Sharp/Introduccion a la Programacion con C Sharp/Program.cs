83c2372dc6ee599c94ea0638fcbe05a524a99143
Luis Coronel <lucho_9292@live.com>
2018-07-21 22:00:05 -0300

    Arreglo la estructura de proyecyos C# 1

---
+++ Introduccion a la Programacion con C Sharp/Introduccion a la Programacion con C Sharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduccion_a_la_Programacion_con_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Esto con java no pasaba");
+            Console.ReadKey();
+
+            List<string> nombres;
+        }
+    }
+}
