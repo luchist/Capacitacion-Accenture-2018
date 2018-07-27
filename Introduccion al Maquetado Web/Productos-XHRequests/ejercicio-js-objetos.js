601dadb08c1f4d965c4c6229b4a59d311787639a
Luis Coronel <lucho_9292@live.com>
2018-07-27 15:35:36 -0300

    update compras - eventos, estilo, login

--- Introduccion al Maquetado Web/Productos-XHRequests/ejercicio-js-objetos.js
+++ Introduccion al Maquetado Web/Productos-XHRequests/ejercicio-js-objetos.js
@@ -1,33 +1,9 @@
 
-let producto1 = {
-    nombre:"Producto 1",
-    precio:200,
-    color:"rojo",
-    link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1Wi0RZNscIe8QSM53dqlf8-cn86bm9evkxa7aPTHr2PUMaQP7',
-    descripcion:"descripcion del producto"
-}
-let producto2 = {
-    nombre:"Producto 2",
-    precio:200,
-    color:"rojo",
-    link: 'https://static.mercadoshops.com/flipazoo-original-peluches-en-transformalos-quinotoys_iZ963306825XsZ69121590XpZ13XfZ69121590-672647290-13XvZgrandexIM.jpg?v=master-20180720_085242',
-    descripcion:"descripcion del producto"
-}
-let producto3 = {
-    nombre:"Producto 3",
-    precio:200,
-    color:"rojo",
-    link: 'https://i.ebayimg.com/images/g/vdkAAOSwOfdZsZ~Y/s-l300.jpg',
-    descripcion:"descripcion del producto"
-}
-
-let productos = [producto1, producto2, producto3]
-
 /*me traigo la seccion de informacion*/
 //let info = document.getElementById('info')
 let info = document.querySelector("#info")
 let contenedor = document.querySelector("#info")
-console.log(info)
+
 
 console.log("VOY A REALIZAR UNA PETICION XMLHttpRequest!")
 var xhr = new XMLHttpRequest()
@@ -38,6 +14,7 @@ xhr.addEventListener("load",function(){
         //console.log(JSON.parse(xhr.responseText)[0].nombre)
         var productos = JSON.parse(xhr.responseText)
         load_elements(productos)
+        //assign_hover(productos)
     }
 })
 xhr.send()
@@ -48,9 +25,111 @@ function load_elements(productos){
     for (var i = 0;i < cantidad; i++){
         let prod_div   = "<div class='contenedor'>"
         let prod_img   = "<p> <img src= " + productos[i].link + " ></p>"
-        let prod_name  = "<p> Nombre: " + productos[i].nombre + "</p>"
-        let prod_price = "<p> Precio : $"+ productos[i].precio + "</p>"
-        let prod_color = "<p> Color: " + productos[i].color + "</p></div>"
+        let prod_name  = "<p class='highlight'>Nombre</p><p> " + productos[i].nombre + "</p>"
+        let prod_price = "<p class='highlight'>Precio</p><p> $"+ productos[i].precio + "</p>"
+        let prod_color = "<p class='highlight'>Color</p><p> " + productos[i].color + "</p></div>"
         contenedor.innerHTML += prod_div + prod_img + prod_name + prod_price + prod_color
     }
 }
+
+/*agarro todos los productos y les pongo la accion onHover*/
+/*
+function assign_hover(productos){
+    let items = document.getElementsByClassName('contenedor')
+    let cantidad = productos.length
+    //
+    console.log(cantidad)
+    console.log("VOY A IMPRIMIR TODOS LOS PRODUCTOS")
+    console.log(items)
+    for (var i = 0; i < cantidad; i++) {
+        var prod = items[i]
+        console.log(prod)
+        prod.addEventListener("click", function(e){
+            prod.innerText = productos[i].descripcion
+            console.log("Agregue " + "productos" + " al contenedor!!!")
+        })
+    }
+}*/
+
+
+/*
+ * Ejercicio: Forms
+ */
+let inputs = document.getElementsByTagName('input')
+/*
+form.addEventListener("mouseout",function(e){
+    console.log("ME FUI DEL FORM")
+})*/
+
+function asignar_eventos_mouse(object){
+    object.onmouseout = function(){
+        console.log("mouse_out")
+    }
+    object.onmouseover = function(){
+        console.log("mouse_over")
+    }
+    object.onblur = function(){
+        console.log("blur")
+    }
+}
+
+let input_nombre = document.getElementById('form_nombre')
+let input_pass = document.getElementById('form_password')
+let btn_submit = document.getElementById('submit')
+
+/*
+Me traigo la BD de usuarios
+*/
+
+console.log("VOY A REALIZAR UNA PETICION XMLHttpRequest!")
+var xhr2 = new XMLHttpRequest()
+xhr2.open("GET","usuarios.json")
+xhr2.addEventListener("load",function(){
+    if(xhr2.status == 200){
+        var usuarios = JSON.parse(xhr2.responseText)
+        evento_user(usuarios)
+        evento_pass(usuarios)
+    }
+    else {
+        var usuarios = "no se pudo traer el JSON!"
+    }
+})
+xhr2.send()
+
+function evento_user(usuarios){
+    input_nombre.addEventListener("blur",function(e){
+        //buscar en la lista de usuarios
+        var found = usuarios.some(function (el) {
+            return el.usuario === input_nombre.value;
+        });
+        if (found){
+            input_nombre.style.backgroundColor = "#3EB991";
+            console.log("El usuario es correcto!")
+        } else{
+            input_nombre.style.backgroundColor = "#EF334C";
+            console.log("El usuario no existe!")
+        }
+    })
+}
+
+function evento_pass(usuarios){
+    input_pass.addEventListener("blur",function(e){
+        //buscar en la lista de usuarios
+        var found = usuarios.some(function (el) {
+            return el.contraseña === input_pass.value;
+        });
+        if (found){
+            input_pass.style.backgroundColor = "#3EB991";
+            console.log("La contraseña es correcta!")
+        } else{
+            input_pass.style.backgroundColor = "#EF334C";
+            console.log("La contraseña es incorrecta!")
+        }
+    })
+}
+
+btn_submit.addEventListener("submit",function(e){
+    //e.preventDefault()
+    console.log("ME FUI A GOOGLE!")
+})
+
