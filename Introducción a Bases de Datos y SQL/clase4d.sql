9df81ba22452240494032a3c5cbc0deb9b244d11
Luis Coronel <lucho_9292@live.com>
2018-07-30 15:24:09 -0300

    Cuarta clase .- CREATE INSERT UPDATE :snake:
    Microsoft SQL Server Management Studio - pubs

---
+++ Introducción a Bases de Datos y SQL/clase4d.sql
@@ -0,0 +1,93 @@
+USE [pubs]
+GO
+CREATE TABLE NuevaSucursal(
+	stor_id char(4) NOT NULL,
+	stor_name varchar(40) NULL,
+	stor_address varchar(40) NULL,
+	city varchar(20) NULL,
+	state char(2) NULL,
+	zip char(5) NULL)
+GO
+--me crea la tabla nueva sucursal, por ahora vacia
+
+INSERT NuevaSucursal (stor_id, stor_name, stor_address, city, state, zip) VALUES ('7890', 'Sucursal Rosario', 'Calle 1', 'Rosario', 'SF', '1000')
+INSERT NuevaSucursal (stor_id, stor_name, stor_address, city, state, zip) VALUES ('1234', 'Sucursal CABA', 'Calle 2', 'CABA', 'BA', '2000')
+
+select * from NuevaSucursal; --tenemos las dos sucursales nuevas
+
+select * from stores; -- la tabla stores no cambio
+
+
+--uno los dos resultados en una sola tabla
+select * from NuevaSucursal --tenemos las dos sucursales nuevas
+union
+select * from stores;
+
+
+-- los inserto en la tabla stores
+-- isnercion de las novedades
+insert stores
+select stor_id, stor_name, stor_address, city,state, zip from NuevaSucursal
+
+select * from stores;
+--Esto se conoce como tablas permanentes.
+
+-- Select Into
+USE pubs
+SELECT title_id as Codigo, title AS Libro,
+       price AS PrecioNeto,
+	   (price * 1.21) AS PrecioConIva
+INTO #ListaPrecios						--las tablas que comienzan con # son tablas temporales locales
+FROM titles
+--Ver tabla creada en TempDB, temporary tables
+-- Esto solo vive mientras la conexion local este viva
+
+select * from #ListaPrecios
+-- Al cerrar la query, la tabla se elimina automaticamente de la base de datos
+-- Muy util para resultados intermedios, cuando no tenemos permisos para modificar la base de datos, etc
+
+-- Si queremos que sea global usamos deos numerales ##
+-- La diferencia entre local y global es que a la global la ven todas las conexiones
+
+
+/*
+Insercion de Datos Parciales
+*/
+
+
+/*
+Eliminacion de Datos
+*/
+
+--DELETE vs TRUNCATE
+
+--Delete asigna los campos con un flag para ser desasignados
+--Truncate desasigna la tabla completa
+
+-- Eliminacion basada en otras tablas
+
+
+--Eliminar el vinculo de titleauthor para los libros de fecha 09-06-1991
+delete from titleauthor
+from titles as t
+inner join titleauthor as ta
+on t.title_id=ta.title_id
+where pubdate='19910609'
+
+select * from titles as t
+inner join titleauthor as ta
+on t.title_id = ta.title_id
+where pubdate='19910609'
+--para chequear si realmente los elimino, primero puedo hacer un select con la misma
+--condicion, despues elimino y vuelvo a chequer
+
+/*
+ACTUALIZACION DE FILAS BASADO EN DATOS DE LA TABLA
+*/
+
+select * from titles;
+
+use pubs;
+update titles
+	set price = (price * 1.1) --SET especifica los datos nuevos
+--puedo usar WHERE para restriingir los datos que quiero actualizar
